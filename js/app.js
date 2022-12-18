eaff159335b4b5bfb28cb9755803c66993c2e1d9
EricCSheppard <EricCSheppard@gmail.com>
2022-12-17 20:02:21 -0500

    adds empty functions for clicking squares and reset button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-// console.log('hi')
 
+//
 // turn counter - odd turns and even turns to keep track of X or O
 
 // finds the reset button
 const reset = document.querySelector('#reset')
 const container = document.querySelector('#container')
+
+const playBox = () => {
+    console.log('I clicked a box')
+} 
+
 
 // make game board and squares within
 const initialState = () => {
@@ -16,13 +21,16 @@ const initialState = () => {
         box.classList.add('box')
         container.appendChild(box)
         box.setAttribute('id', i)
-
-        // box.addEventListener('click', playBox)
+        box.addEventListener('click', playBox)
     }
 }
 
+const resetGame = () => {
+    console.log('pressed reset button')
+}
+
 // runs the reset function when reset button is clicked
-reset.addEventListener('click', initialState)
+reset.addEventListener('click', resetGame)
 
 
 // loads the initial state once page is loaded
