5fb2a2090db8fbac1a25bc2ddf9465652d0a090a
EricCSheppard <EricCSheppard@gmail.com>
2022-12-20 21:43:44 -0500

    adds win condition testing

--- js/app.js
+++ js/app.js
@@ -1,24 +1,10 @@
-// A user should be able to click on different squares to make a move.
-// Every click will alternate between marking an X and O
-// Upon marking of an individual cell, use JavaScript to add an X or O to the cell, according to whose turn it is.
-// A cell should not be able to be replayed once marked.
-// You should not be able to click remaining empty cells after the game is over.
-// Add a reset button that will clear the contents of the board.
-// Display a message to indicate which turn is about to be played.
-// Detect draw conditions (ties/cat's game)
-// Detect winner: Stop game and declare the winner if one player ends up getting three in a row.
-// Hint: Determine a set of winning combinations. Check those combinations on the board contents after every move.
-//
-// turn counter - odd turns and even turns to keep track of X or O
-
-// finds the reset button
 const reset = document.querySelector('#reset')
 const container = document.querySelector('#container')
 const xClass = 'X'
 const oClass = 'O'
 let moves = 1
 let playerX = true
-let winCon = [
+const winCon = [
     ['1', '2', '3'],
     ['4', '5', '6'],
     ['7', '8', '9'],
@@ -31,24 +17,19 @@ let winCon = [
 let playerOMoves = []
 let playerXMoves = []
 
-
 const playBox = (event) => {
     const box = event.target
     const currentClass = playerX ? xClass : oClass
     placeMark(box, currentClass)
     if (playerX === false) {
-    playerOMoves.push(event.target.id)
+        playerOMoves.push(event.target.id)
     } else {
         playerXMoves.push(event.target.id)
     }
-    console.log(box.innerHTML)
-    // checkWinCondition()
+    checkWin()
     moves ++
-    // console.log(playerX)
     playerTurn()
-    console.log(playerOMoves)
-    console.log(playerXMoves)
-    // console.log('Number of moves:', moves)
+    printWin(moves, checkWin())
 } 
 
 const placeMark = (box, currentClass) => {
@@ -63,10 +44,35 @@ const playerTurn = () => {
     } 
 }
 
+const checkWin = () => {
+    for(i = 0; i < winCon.length; i++) {
+        if(findWinCombo(playerXMoves, winCon[i]) === true) {
+            return true
+        } else if (findWinCombo(playerOMoves, winCon[i]) === true) {
+            return false
+        } 
+    }
+    return null
+}  
 
+const printWin = (moves, result) => {
+    if (result === true | result === false | (result === null && moves === 9)) {
+        if (result === true) {
+            gameOver()
+        } else if (result === false) {
+            gameOver()
+        } else if (moves === 9 && result === null) {
+            resetGame()
+        }
+    } 
+}
+const findWinCombo = (array1, array2) => {
+    const checkFor = (box) => {
+        return array1.includes(box)
+    }
+    return array2.every(checkFor) 
+}
 
-
-// make game board and squares within
 const initialState = () => {
     while (container.firstChild) {
         container.removeChild(container.firstChild)
@@ -77,13 +83,16 @@ const initialState = () => {
         container.appendChild(box)
         box.setAttribute('id', i)
         box.addEventListener('click', playBox)
-        
     }
+}
+
+const gameOver = () => {
+    alert('game over')
 }
 
 
 const resetGame = () => {
-    console.log('pressed reset button')
+    // console.log('pressed reset button')
     moves = 1
     playerX = true
     initialState()
@@ -91,11 +100,8 @@ const resetGame = () => {
     playerXMoves = []
 }
 
-// runs the reset function when reset button is clicked
 reset.addEventListener('click', resetGame)
 
-
-// loads the initial state once page is loaded
 document.addEventListener('DOMContentLoaded', () => {
         initialState()
 })
